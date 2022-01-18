af4b69ad6da9b9a9c7aa83b1c8f786134679105b
Jennifer Hoang <jennifer.anh.hoang@gmail.com>
2022-01-17 20:46:31 -0800

    Doc: add plot_histogram function specification

---
+++ R/plot_histogram.R
@@ -0,0 +1,14 @@
+#' Creates histograms for numerical features within a dataframe using ggplot2
+#'
+#' @param data A dataframe
+#' @param columns A list of numerical features for which to create histograms,
+#' or by default will plot all numerical features in dataframe.
+#' @param num_bins Integer. Number of bins per histogram plot, default is 30 bins.
+#'
+#' @return ggplot object
+#' @export
+#'
+#' @examples
+plot_histogram <- function(data, columns, num_bins = 30) {
+
+}
