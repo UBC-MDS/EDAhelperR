d5d70e572033c9c5802601fce9daba0f9b4008de
Steven Leung <klsleung@student.ubc.ca>
2022-01-18 23:12:28 -0800

    roxygen2 formatted functional specs of numeric_plots().

---
+++ R/numeric_plots.R
@@ -0,0 +1,17 @@
+library(roxygen2)
+library(GGally)
+
+#' Generate scattered plot matrix of numeric features for EDA
+#'
+#' @param df A data.frame
+#'
+#' @return a GGally plot object with the scattered plot matrix of numeric
+#'         features.
+#' @export
+#'
+#' @examples
+#' numeric_plots(df)
+numeric_plots <- function(df){
+
+}
+
