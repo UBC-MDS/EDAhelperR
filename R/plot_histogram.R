277638ebd55686ee3f30ef86980abc71d7ffd660
Jennifer Hoang <jennifer.anh.hoang@gmail.com>
2022-01-26 21:22:16 -0800

    Tests: add exception handling for plot_histogram.R

--- R/plot_histogram.R
+++ R/plot_histogram.R
@@ -9,6 +9,32 @@
 #' @export
 #'
 #' @examples
-plot_histogram <- function(data, columns, num_bins = 30) {
+plot_histogram <- function(data, columns = "all", num_bins = 30) {
+  # Exception handling
+  if (!is.data.frame(data))
+    stop("data must be a data frame.")
+  if (!is.vector(columns))
+    stop("columns must be a vector of column names within data frame.")
+  if (!is.numeric(num_bins))
+    stop("num_bins must be a numeric value.")
 
+  # Select columns from data
+  if (columns == "all"){
+    plot_data <- data |>
+      dplyr::select_if(is.numeric)
+    columns <- colnames(plot_data)
+  } else {
+    plot_data <- data |>
+      dplyr::select(columns)
+  }
+
+  # Generate plots
+  plot_list <- list()
+
+  for (i in seq_along(plot_data)) {
+    plot_list[[i]] <- ggplot2::ggplot(plot_data, aes(x = columns[i])) +
+      geom_histogram(bins = num_bins)
+  }
+  cowplot::plot_grid(plotlist = plot_list, ncol = 3)
 }
+
