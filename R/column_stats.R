cff4bdc62ed32b6221801a37f41702c723c3ede3
superNOVA <jinglu_cui@163.com>
2022-01-31 22:42:07 -0800

    Fix workflow

--- R/column_stats.R
+++ R/column_stats.R
@@ -1,7 +1,6 @@
 #' Obtain summary statistics of column(s) including count, mean, median, mode, Q1, Q3,
-#'variance, standard deviation, correlation, and covariance in table format.
-#' Parameters
-#' -------------
+#' variance, standard deviation, correlation, and covariance in table format.
+#'
 #' @param data A data set from which columns will be selected
 #' @param columns A list of columns for which to obtain summary stats, correlation matrix, and covariance matrix
 #'           (if > 1 column arguments used)
